9481b4a250dd9dff9121c7ee683ed7141521ba99
Eric Rion <erion42@gmail.com>
2021-11-15 23:04:29 -0500

    Can log in, log out, and type in a post. It doesn't LOOK good, but it does the basic things. If I can figure out how to make the data read and write I'll re-submit it

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -60,7 +60,8 @@ router.get('/dashboard', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
+    res.render('all-posts', {
+      layout: "dashboard",
       ...user,
       logged_in: true
     });
